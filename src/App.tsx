30abcaaa65c71008366644551fca4c0f16850808
PainQlers <167845645+PainQlers@users.noreply.github.com>
2025-08-25 14:08:52 +0700

    feat:Complete ex.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
+  <Greeting name="john" age={21}/>
   return;
 }
 
--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -2,16 +2,20 @@
 import { useState } from "react";
 
 // TODO1:  ประกาศ Type ด้วย type หรือ inerface
+interface greetingProps {
+  name: string,
+  age: number
+}
 
 // TODO2: กำหนด Type ให้ props
-const Greeting = (props) => {
+const Greeting = ({name, age}:greetingProps) => {
   // TODO3: กำหนด Type ให้ useState
   const [count, setCount] = useState();
 
   return (
     <div>
       <h1>
-        Hello {props.name}, you are {props.age} years old 👋
+        Hello {name}, you are {age} years old 👋
       </h1>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click Me</button>
